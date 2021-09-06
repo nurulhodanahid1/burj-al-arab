6aa481f12711ad72428ba1ab072b8515436c4a15
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-09-06 11:57:21 +0600

    start the projet

---
+++ src/components/Book/Book.js
@@ -0,0 +1,17 @@
+import { colors } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+
+const Book = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const {bedType} = useParams();
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>Hello, <span style={{color: "red"}}>{loggedInUser.name}</span>! Let's book a {bedType} Room.</h1>
+            <p>Want a <Link to="/home">different room?</Link> </p>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwAGmvLMv5pVA85VWpCcqIwsGsIbCKTyI",
+  authDomain: "burj-al-arab-e17d2.firebaseapp.com",
+  projectId: "burj-al-arab-e17d2",
+  storageBucket: "burj-al-arab-e17d2.appspot.com",
+  messagingSenderId: "647990737439",
+  appId: "1:647990737439:web:5575017c2683d60ae6f32a"
+};
+  export default firebaseConfig;
